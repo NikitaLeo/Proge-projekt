d6597696fde4d9b169d2d74ff77bd56eaab0f2b0
Nikita Leo <nleoest@gmail.com>
2019-03-16 20:35:37 +0200

    added a comment

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.io.File;
 import javax.swing.JOptionPane;
 
+// comment
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Info");
